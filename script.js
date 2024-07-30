010c487fa987618d59a0dfb578e7563300f95ed2
Liaoxyu <161911756+LiaoxyuCM@users.noreply.github.com>
2024-07-30 18:56:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function checkInput(){
 	var inputContent = document.getElementById('inputPassword').value;
 	if (inputContent === '18,161,130') {
-    document.getElementById('inputPassword').value = '输入正确';
+		document.getElementById('inputPassword').value = '输入正确';
 		setTimeout(function (){
 			window.location.replace('https://hmpg.net/');
 		}, 2000);
